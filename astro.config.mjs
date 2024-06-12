87b24d0c2bde08b0e89a838f8fea714b6fe7bab0
Manoj <chapagainmanoj35@gmail.com>
2024-06-12 19:01:14 -0400

    Staging pushed to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://mostavio.com'
+  site: 'https://mostavio.com',
+  output: 'server',
+  adapter: vercel(),
 });
