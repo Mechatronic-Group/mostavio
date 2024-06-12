41282eaa00275476c5b724784c4e9643e7e13d83
Manoj <chapagainmanoj35@gmail.com>
2024-06-12 18:38:54 -0400

    First commit

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'MostAvio - The Future of Aviation';
+export const SITE_DESCRIPTION = 'Website for MostAvio';
+export const SITE_URL = 'https://mostavio.com';
+export const SITE_IMAGE = '/og.png';
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+import themer from "@tailus/themer";
+
+module.exports = {
+    content: [
+        './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+        "./node_modules/@tailus/themer-**/dist/**/*.{js,ts}"
+    ],
+    darkMode: 'media', // or 'media' or 'class'
+    plugins: [
+        themer({
+            palette: {
+                extend: "oz",
+            },
+            radius: "smoothest",
+            background: "light",
+            border: "light",
+            padding: "large"
+        })
+    ],
+};
