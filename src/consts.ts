98a733af3c23adda4d7fc063326bae3e83c30a69
Manoj <chapagainmanoj35@gmail.com>
2024-06-13 11:17:27 -0400

    Hero page done

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,28 @@ export const SITE_TITLE = 'MostAvio - The Future of Aviation';
 export const SITE_DESCRIPTION = 'Website for MostAvio';
 export const SITE_URL = 'https://mostavio.com';
 export const SITE_IMAGE = '/og.png';
+
+
+export const internalLinks = [
+    { title: "Home", url: "#" },
+    { title: "About", url: "#" },
+    { title: "Guide", url: "#" },
+    { title: "Blocks", url: "#" },
+    { title: "Contact", url: "#" },
+    { title: "Terms of Use", url: "#" },
+];
+
+import FacebookIcon from "@components/Icons/FacebookIcon.astro";
+import InstagramIcon from "@components/Icons/InstagramIcon.astro";
+import LinkedInIcon from "@components/Icons/LinkedinIcon.astro";
+import TwitterIcon from "@components/Icons/TwitterIcon.astro";
+import YouTubeIcon from "@components/Icons/YoutubeIcon.astro";
+
+
+export const externalLinks = [
+    { title: "Facebook", url: "#", icon: FacebookIcon },
+    { title: "Instagram", url: "#", icon: InstagramIcon },
+    { title: "Twitter", url: "#", icon: TwitterIcon },
+    { title: "YouTube", url: "#", icon: YouTubeIcon },
+    { title: "LinkedIn", url: "#", icon: LinkedInIcon },
+];
