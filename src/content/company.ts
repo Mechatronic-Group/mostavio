801a6e3e0b554c69fc8b1f2d2bb3e4448e1307c4
Manoj <chapagainmanoj35@gmail.com>
2025-07-23 12:37:17 -0400

    Update things

--- src/content/company.ts
+++ src/content/company.ts
@@ -1,19 +1,11 @@
-import step1Image from "../assets/timeline/step1.webp";
-import step2Image from "../assets/timeline/step2.webp";
-import step3Image from "../assets/timeline/step3.webp";
-import step4Image from "../assets/timeline/step4.webp";
-import member from "../assets/team/member2.webp";
+
 import museWinnerImage from "../assets/timeline/muse_winner.png";
 import goAeroLogo from "../assets/timeline/goaeroLogo.png";
 import skyTechLogo from "../assets/SkyTechTransparent.png"
 
-import TrophyIcon from "../assets/icons/trophy.svg?raw";
-import RocketIcon from "../assets/icons/rocket.svg?raw";
-import LightBulbIcon from "../assets/icons/lightbulb.svg?raw";
-import BeakerIcon from "../assets/icons/beaker.svg?raw";
-import WrenchIcon from "../assets/icons/wrench-screwdriver.svg?raw";
-import CubeIcon from "../assets/icons/cube.svg?raw";
-import UserPlusIcon from "../assets/icons/user-plus.svg?raw";
+
+// Assets for v2
+import RedDotAward from "../assets/v2/company/RedDotAward.png";
 
 
 let bannerContent = {
@@ -72,68 +64,19 @@ let jobList = [
         link: "mailto:info@mostavio.com",
     },
     {
-        title: "Aerospace Engineer",
+        title: "HR Manager",
         location: "Toronto, ON",
-        team: "Engineering",
+        team: "Administration and Support",
         description:
-            "We are seeking an Aerospace Engineer to help us design and develop our eVTOL aircraft.",
+            "Manage recruitment and onboarding processes \
+            Develop and implement HR policies and procedures \
+            Foster a positive work environment and company culture.",
         link: "mailto:info@mostavio.com",
     },
 ];
 
 // For reference
 
-// const timeline = [
-//     {
-//         year: "2019",
-//         title: "Founded by Ariane Doe",
-//         description:
-//             "Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia",
-//         image: step1Image.src,
-//         avatarImg: "/images/avatars/avatar.webp",
-//         highlight: {
-//             type: "customers",
-//             text: "12 Satisfied",
-//             link: { href: "customers.html", text: "Customers" },
-//         },
-//     },
-//     {
-//         year: "2020",
-//         title: "Grown the team",
-//         description:
-//             "Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia",
-//         image: step2Image.src,
-//         highlight: {
-//             type: "team",
-//             text: "5 Developers joined the",
-//             link: { href: "customers.html", text: "Team" },
-//             members: [member.src, member.src, member.src],
-//         },
-//     },
-//     {
-//         year: "2021",
-//         title: "Startup of the year",
-//         description:
-//             "Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia",
-//         image: step3Image.src,
-//         highlight: {
-//             type: "companies",
-//             text: "Worked with 3 world leading",
-//             link: { href: "customers.html", text: "companies" },
-//             clients: [
-//                 airbusImage.src, brailleImage.src, ontarioImage.src,
-//             ],
-//         },
-//     },
-//     {
-//         year: "2022",
-//         title: "Founded by Bernard Ng",
-//         description:
-//             "Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia",
-//         image: step4Image.src,
-//         highlight: null,
-//     },
-// ];
 
 const timeline = [
     {
@@ -141,9 +84,6 @@ const timeline = [
         title: "MOSTAVIO Founded",
         description:
             "Founded with the ambition to revolutionize personal air mobility. Infrastructure development and strategic planning began.",
-        // image: step1Image.src,
-        // avatarImg: RocketIcon.src,
-        avatar: RocketIcon,
         highlights: null,
     },
     {
@@ -151,8 +91,6 @@ const timeline = [
         title: "MX1 Concept Designs Unveiled",
         description:
             "First concept designs of the MX1 eVTOL were revealed.",
-        avatar: LightBulbIcon,
-        image: step2Image.src,
         highlights: null,
     },
     {
@@ -160,36 +98,24 @@ const timeline = [
         title: "Grown the team",
         description:
             "We expanded our team with new aerospace, mechanical, and software engineers.",
-        // image: step2Image.src,
-        avatar: UserPlusIcon,
-        highlights: [{
-            type: "team",
-            text: "New Engineers joined",
-            link: { href: "/company/#team", text: "Our Team" },
-            images: [member.src, member.src, member.src],
-        }],
     },
     {
         year: "2023",
         title: "MX1 Testing and Component Development",
-        avatar: BeakerIcon,
         description:
             "Completed subscale testing and developed key components for MX1.",
-        // image: step3Image.src,
         highlights: null,
     },
     {
         year: "2024",
         title: "Expanded with Prototype Assembly and new division",
-        avatar: WrenchIcon,
         description:
             "MX1 prototype fully assembled. Launched Skytech division and became an official DJI enterprise dealer.",
 
         highlights: [
             {
-                type: "companies",
-                text: "Launched Skytech division",
-                link: { href: "https://skyt.ca/", text: "Skytech", target: "_blank" },
+                text: "Launched Skytech division.",
+                link: { href: "https://skyt.ca/", text: "Find Out More", target: "_blank" },
                 images: [
                     skyTechLogo.src
                 ],
@@ -199,30 +125,16 @@ const timeline = [
     {
         year: "2025",
         title: "Breakthrough Year",
-        avatar: TrophyIcon,
         description:
             "Successful eVTOL flight test. Winner of GOAERO Competition Stage 1 and Muse Design Award for eVTOL \
              innovation. Recognized by Transport Canada as an RPAS Flight School. Published NSERC-backed research \
             with Centennial College.",
-        // image: step4Image.src,
         highlights: [
             {
-                type: "companies",
-                text: "Winner of GOAERO & MUSE Awards",
-                link: { href: "/news/mostavio-wins-muse-design-award", text: "Learn more" },
+                text: "Check our news section to find out more.", link: { href: "/news/mostavio-wins-muse-design-award", text: "See Here", target: "_blank" },
                 images: [
-                    museWinnerImage.src, goAeroLogo.src
+                    goAeroLogo.src, museWinnerImage.src, RedDotAward.src
                 ]
-            },
-            {
-                type: "customers",
-                text: "Recognized by Transport Canada as an RPAS Flight School",
-                link: { href: "https://skyt.ca/training-and-certification/", text: "Find out more", target: "_blank" },
-            },
-            {
-                type: "customers",
-                text: "Published NSERC-backed research with Centennial College",
-                link: { href: "customers.html", text: "Link" },
             }
         ],
     },
