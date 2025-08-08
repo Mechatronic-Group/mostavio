87057eaf163e828a2fc0683ceba30e23b4e1986b
Manoj <chapagainmanoj35@gmail.com>
2025-08-08 11:57:25 -0400

    Header fix

--- src/consts.ts
+++ src/consts.ts
@@ -13,9 +13,9 @@ export const internalLinks = [
 ];
 
 export const companyLinks = [
-    { title: "About Us", url: "/company#about" },
+    { title: "About Us", url: "/company/#about" },
     // { title: "News", url: "/news" },
-    { title: "Careers", url: "/company#careers" },
+    { title: "Careers", url: "/company/#careers" },
     { title: "Contact", url: "/contact" }
 ];
 
